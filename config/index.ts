11a37bdcc978baf45829d3a4431d8bb4acbe6c5f
farpires <arpirez.fabio@gmail.com>
2023-01-08 23:52:30 -0300

    Add: environment variable has been added

--- config/index.ts
+++ config/index.ts
@@ -7,6 +7,12 @@ if (process.env.NODE_ENV !== "production") {
 
 export default {
     PORT: process.env.PORT,
-    MONGO: process.env.MONGO
-  
+    MONGO: process.env.MONGO,
+    DATABASE: {
+        database: process.env.DATABASE,
+        username: process.env.USER_NAME,
+        password: process.env.PASSWORD,
+        host: process.env.HOST_NAME,
+        dialect: process.env.DIALECT,
+      },
 }
