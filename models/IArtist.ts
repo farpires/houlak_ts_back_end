b7d5036004851a1082f10f20d6fb78de129d9b58
farpires <arpirez.fabio@gmail.com>
2023-01-10 02:16:43 -0300

    Add: typescript models has been added

---
+++ models/IArtist.ts
@@ -0,0 +1,29 @@
+export type TGetArtistData = IArtistData[] | Error;
+
+export interface IArtistData {
+    artists: Artist[];
+    images: Image[];
+    label: string;
+    name: string;
+    popularity: number;
+}
+
+interface Artist {
+    external_urls: ExternalUrls;
+    href: string;
+    id: string;
+    name: string;
+    type: string;
+    uri: string;
+}
+
+interface ExternalUrls {
+    spotify: string;
+}
+
+interface Image {
+    height: number;
+    url: string;
+    width: number;
+}
+
